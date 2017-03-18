f9a24de41db394a3d1997e1e05445bb3225736fa
Ilya Kochergin <tigro2004@mail.ru>
2017-03-18 13:53:50 +0300

    posle 3pary

--- egypt.py
+++ egypt.py
@@ -1,2 +1,7 @@
-mandarine = "я желтый египетский мандарин"
+﻿mandarine = "я желтый египетский мандарин"
 potato = "я египетская картошка"
+
+if __name__ == '__main__' :
+	print(' module egypt in main program')
+else:
+	print('module egypt was imported')
